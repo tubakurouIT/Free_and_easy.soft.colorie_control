09172460ba3f72da8b769dc0c01b169bbe66852d
tubakurouIT <tubakurou4maruti@yahoo.co.jp>
2025-01-22 11:43:18 +0000

    favorites

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_01_20_073231) do
+ActiveRecord::Schema.define(version: 2025_01_22_081447) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -59,6 +59,13 @@ ActiveRecord::Schema.define(version: 2025_01_20_073231) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
+    t.integer "member_id"
+    t.integer "free_post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
     t.integer "member_id"
     t.integer "free_post_id"
     t.datetime "created_at", precision: 6, null: false
